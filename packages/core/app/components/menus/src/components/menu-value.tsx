d69a86d1b12106431478b310298623a66e222526
Wester <wester@choiceform.com>
2026-04-25 13:32:06 +0800

    refactor(menus): convert MenuValue to forwardRef for ref forwarding

--- packages/core/app/components/menus/src/components/menu-value.tsx
+++ packages/core/app/components/menus/src/components/menu-value.tsx
@@ -1,18 +1,21 @@
-import { HTMLProps, memo, ReactNode } from "react"
+import { forwardRef, HTMLProps, memo, ReactNode } from "react"
 
 export interface MenuValueProps extends HTMLProps<HTMLSpanElement> {
   children: ReactNode
 }
 
-export const MenuValue = memo(({ children, ...rest }: MenuValueProps) => {
-  return (
-    <span
-      className="flex-1 cursor-default truncate select-none"
-      {...rest}
-    >
-      {children}
-    </span>
-  )
-})
+export const MenuValue = memo(
+  forwardRef<HTMLSpanElement, MenuValueProps>(({ children, ...rest }, ref) => {
+    return (
+      <span
+        ref={ref}
+        className="flex-1 cursor-default truncate select-none"
+        {...rest}
+      >
+        {children}
+      </span>
+    )
+  }),
+)
 
 MenuValue.displayName = "MenuValue"
